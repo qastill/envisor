d91c8aa1a944630d067896e92d04b8038ef53c03
qastill <qastill@gmail.com>
2026-04-06 10:14:31 +0700

    fix: bump SW cache to v4 to invalidate old cached HTML

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'envisor-v3';
+const CACHE_NAME = 'envisor-v4';
 const STATIC = ['/', '/css/style.css', '/js/app.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
